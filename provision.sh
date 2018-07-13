3f6cd671eb8fa6c2446fc7d712b81b3b2a027281
Stephen Carter <stephencarter@gmail.com>
2018-07-13 22:40:40 +0100

    Adding initial files

---
+++ provision.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+RABBITMQ_VERSION=3.7.7-1
+RABBITMQ_USERNAME=vcap
+RABBITMQ_PASSWORD=changeme
+RABBITMQ_ERLANG_COOKIE=bugsbunny
+
+# Copy hosts file so we can resolve all nodes by name
+echo "Updating hosts file"
+cp /vagrant/hosts /etc/hosts
+
+# Install Erlang repo
+echo "Adding Erlang repo"
+wget -O ~/erlang-solutions_1.0_all.deb https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
+dpkg -i ~/erlang-solutions_1.0_all.deb
+
+# Install PackageCloud RabbitMQ repo
+echo "Adding RabbitMQ repo"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.deb.sh | bash
+
+# Update apt metadata
+# No need to do this for now since the PackageCloud script automatically runs an update
+# apt-get update
+
+# Install specific RabbitMQ version
+echo "Installing RabbitMQ"
+apt-get --yes install rabbitmq-server=$RABBITMQ_VERSION
+
+echo "Setting Erlang Cookie"
+echo $RABBITMQ_ERLANG_COOKIE > /var/lib/rabbitmq/.erlang.cookie
+service rabbitmq-server stop
+service rabbitmq-server start
+
+# Enable mgmt plugin
+echo "Enabling rabbitmq_management plugin"
+rabbitmq-plugins enable rabbitmq_management
+
+# Create a new user with admin rights
+echo "Adding user \"$RABBITMQ_USERNAME\""
+rabbitmqctl add_user $RABBITMQ_USERNAME $RABBITMQ_PASSWORD
+rabbitmqctl set_permissions -p / $RABBITMQ_USERNAME ".*" ".*" ".*"
+rabbitmqctl set_user_tags $RABBITMQ_USERNAME administrator
+
+echo "Showing RabbitMQ status"
+rabbitmqctl status
+
+HOSTNAME=`hostname`
+case "$HOSTNAME" in
+    rabbitmq2|rabbitmq3)
+        echo "Stopping RabbitMQ and joining cluster"
+        rabbitmqctl stop_app
+        rabbitmqctl join_cluster rabbit@rabbitmq1
+        rabbitmqctl start_app
+
+        # Run a cluster status to verify node joined
+        echo "Showing RabbitMQ cluster status"
+        rabbitmqctl cluster_status
+        ;;
+    client1|client2)
+        echo "Installing Apt packages"
+        apt-get --yes install python-pip
+
+        echo "Installing Python packages"
+        pip install pika
+        ;;
+esac;
